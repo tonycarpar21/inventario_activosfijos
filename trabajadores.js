68f89e16db92909cce2da1feb189c12af05d2ec0
tonycarpar21 <tonycarpar21@gmail.com>
2026-01-14 17:52:50 -0500

    Add files via upload

---
+++ trabajadores.js
@@ -0,0 +1,122 @@
+window.trabajadores = [
+  {
+    area: "RRHH",
+    nombre: "Daniel León 37"
+  },
+  {
+    area: "RRHH",
+    nombre: "Daniel León 29"
+  },
+  {
+    area: "RRHH",
+    nombre: "Óscar Medina 74"
+  },
+  {
+    area: "RRHH",
+    nombre: "Lucía Flores 47"
+  },
+  {
+    area: "RRHH",
+    nombre: "Lucía Flores 42"
+  },
+  {
+    area: "Sistemas",
+    nombre: "Juan Cruz Saldaña 6"
+  },
+  {
+    area: "Sistemas",
+    nombre: "Miguel Quispe 85"
+  },
+  {
+    area: "Sistemas",
+    nombre: "Patricia Silva 9"
+  },
+  {
+    area: "Sistemas",
+    nombre: "Carmen Valdez 92"
+  },
+  {
+    area: "Sistemas",
+    nombre: "Lucía Flores 31"
+  },
+  {
+    area: "Sistemas",
+    nombre: "Juan Cruz Saldaña 57"
+  },
+  {
+    area: "Administración",
+    nombre: "Ana Gómez 57"
+  },
+  {
+    area: "Administración",
+    nombre: "María Torres 12"
+  },
+  {
+    area: "Administración",
+    nombre: "Andrea Ruiz 20"
+  },
+  {
+    area: "Administración",
+    nombre: "Carlos Rivas 62"
+  },
+  {
+    area: "Logística",
+    nombre: "Elena Chávez 85"
+  },
+  {
+    area: "Logística",
+    nombre: "Rosa Mendoza 45"
+  },
+  {
+    area: "Logística",
+    nombre: "Carlos Rivas 70"
+  },
+  {
+    area: "Logística",
+    nombre: "Patricia Silva 46"
+  },
+  {
+    area: "Logística",
+    nombre: "Paola Ramos 22"
+  },
+  {
+    area: "Producción",
+    nombre: "Óscar Medina 1"
+  },
+  {
+    area: "Producción",
+    nombre: "Carlos Rivas 18"
+  },
+  {
+    area: "Producción",
+    nombre: "Pedro Huamán 14"
+  },
+  {
+    area: "Producción",
+    nombre: "Miguel Quispe 29"
+  },
+  {
+    area: "Producción",
+    nombre: "Ricardo Núñez 64"
+  },
+  {
+    area: "Producción",
+    nombre: "Marco Espinoza 33"
+  },
+  {
+    area: "Producción",
+    nombre: "Pedro Huamán 10"
+  },
+  {
+    area: "Producción",
+    nombre: "Luis Paredes 34"
+  },
+  {
+    area: "Gerencia",
+    nombre: "María Torres 35"
+  },
+  {
+    area: "Gerencia",
+    nombre: "Miguel Quispe 72"
+  },
+];
